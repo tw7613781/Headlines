cdb62f9770a751a50d641f4e4dd2afd61bde7121
tw7613781 <tw7613781@gmail.com>
2018-01-16 00:50:44 +0900

    remove Chinese comtents

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
 #coding:utf-8
+
 import feedparser
 
 from flask import Flask
@@ -7,15 +7,12 @@ from flask import Flask
 app = Flask(__name__)
 
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
-Tencent_FEED = "http://news.qq.com/newsgn/rss_newsgn.xml"
 
 @app.route('/')
 def get_news():
     feed = feedparser.parse(BBC_FEED)
-    feed2 = feedparser.parse(Tencent_FEED)
     # return dictionary including all news storys
     first_article = feed['entries'][0]
-    second_artical = feed2['entries'][0]
     return """<html>
     <body>
         <h1> BBC Headlines </h1>
@@ -23,13 +20,9 @@ def get_news():
         <i>{1}</i> <br/>
         <p>{2}</p> <br/>
         <hr/>
-        <h1> 腾讯国内新闻 </h1>
-        <b>{3}</b> <br/>
-        <i>{4}</i> <br/>
-        <p>{5}</p> <br/>
     </body>
     </html>
-    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"), second_artical.get("title"), second_artical.get("published"), second_artical.get("summary"))
+    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
     # using dict.get() to avoid key missing causing runtime error
 
 if __name__ == '__main__':
