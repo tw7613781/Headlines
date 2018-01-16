fac8dde981ee5c479f6319e2d3bc4b0fa357024b
Max Tang <tw7613781@gmail.com>
2018-01-16 15:28:54 +0900

    initialize project

--- hello.py
+++ hello.py
@@ -3,26 +3,24 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+BBC_FEEDS = {
+    'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn':'http://rss.cnn.com/rss/edition.rss',
+    'fox':'http://feeds.foxnews.com/foxnews/latest',
+    'iol':'http://www.iol.co.za/cmlink/1.640'
+}
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(BBC_FEEDS[publication])
     # return dictionary including all news storys
-    first_article = feed['entries'][0]
-    return """<html>
-    <body>
-        <h1> BBC Headlines </h1>
-        <b>{0}</b> <br/>
-        <i>{1}</i> <br/>
-        <p>{2}</p> <br/>
-        <hr/>
-    </body>
-    </html>
-    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template('home.html',
+                           articles = feed['entries'])
     # using dict.get() to avoid key missing causing runtime error
 
 if __name__ == '__main__':
